b01378aa3965ea11f3cf788a8046d395b796a699
unknown <santiago.nicolas.gomez@estudiante.epsum.school>
2024-11-21 13:10:03 +0100

    falta lo de tipos de dormir, el boton de fav

--- app/src/main/java/com/sngomez27/misaficiones/sobreeMi.java
+++ app/src/main/java/com/sngomez27/misaficiones/sobreeMi.java
@@ -1,29 +1,34 @@
 package com.sngomez27.misaficiones;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.annotation.StringRes;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.tabs.TabLayout;
 import com.sngomez27.misaficiones.databinding.ActivityAficionesBinding;
+import com.sngomez27.misaficiones.databinding.ActivitySobreeMiBinding;
 import com.sngomez27.misaficiones.ui.frmanager.Paginador_SB;
 
 public class sobreeMi extends AppCompatActivity {
 
-    private ActivityAficionesBinding binding;
+    private ActivitySobreeMiBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        binding = ActivityAficionesBinding.inflate(getLayoutInflater());
+        binding = ActivitySobreeMiBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Paginador_SB paginadorSb = new Paginador_SB(this,getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(paginadorSb);
+
+        TabLayout tabLayout = binding.textoSobreeMi;
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
